72ff862fe2fccb387559b291409b2cc2993cfdb3
dev-sai-krishna <saikrishnakrish228@gmail.com>
2024-09-09 23:12:42 +0530

    added new font-family

--- src/components/ImageHolder.jsx
+++ src/components/ImageHolder.jsx
@@ -1,22 +1,27 @@
 // src\pages\Footer.jsx
 /* eslint-disable react/prop-types */
 
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Image = styled.img`
   width: 50px;
   height: 60px;
   padding: 10px;
-`
-const ImageWrapper = styled.div`
-
-`
-const ImageHolder = ({src = "", alt = ""}) => {
+  display: flex;
+  flex-direction: column;
+`;
+const Label = styled.label`
+  font-family: "Celebrate";
+  padding: 0px 10px;
+`;
+const ImageWrapper = styled.div``;
+const ImageHolder = ({ src = "", alt = "" }) => {
   return (
     <ImageWrapper>
       <Image src={src} alt={alt} />
+      <Label>Celebrare</Label>
     </ImageWrapper>
-  )
-}
+  );
+};
 
-export default ImageHolder
+export default ImageHolder;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './redux/store';
 import './index.css';
+import './css/fonts.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
